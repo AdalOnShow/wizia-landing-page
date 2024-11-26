1addd1455eede732874d5af6bfb3c4f598835231
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-11-26 15:18:23 +0600

    create and responsive Service section's Carts

---
+++ src/components/ServiceCart.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+const ServiceCart = ({ image, title, desc }) => {
+    return (
+        <div className="w-full sm:w-[327px] md:w-[384px]">
+            <img src={image} alt={title} className="size-10 md:size-16" />
+            <h2 className="font-semibold text-2xl md:text-[28px] leading-[30px] md:leading-[35px] mt-6 mb-4">{title}</h2>
+            <p className="font-light font-workSans text-lg md:text-[22px] leading-6 md:leading-[29px]">{desc}</p>
+        </div>
+    )
+}
+export default ServiceCart
