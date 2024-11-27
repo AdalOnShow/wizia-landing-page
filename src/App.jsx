c19b2758af98a5d7d101fd69ba3b81c417e4b44d
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-11-27 13:51:55 +0600

    fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import Navbar from "./components/Navbar.jsx";
-import Hero from "./sections/Hero.jsx";
-import Brands from "./sections/Brands.jsx";
-import Service from "./sections/Service.jsx";
-import Train from "./sections/Train.jsx";
-import Reviews from "./sections/Reviews.jsx";
-import Improvement from "./sections/Improvement.jsx";
-import GetStarted from "./sections/GetStarted.jsx";
-import Footer from "./sections/Footer.jsx";
+import {
+    Hero, Brands, Service, Train, Reviews, Improvement, GetStarted, Footer,
+} from './sections'
 
 const App = () => {
-    return (
-        <main className="font-montserrat text-white bg-teal-300">
-            <Navbar />
-            <Hero />
-            <Brands />
-            <Service />
-            <Train />
-            <Reviews />
-            <Improvement />
-            <GetStarted />
-            <Footer />
-        </main>
-    )
+    return (<main className="font-montserrat text-white bg-teal-300">
+        <Navbar />
+        <Hero />
+        <Brands />
+        <Service />
+        <Train />
+        <Reviews />
+        <Improvement />
+        <GetStarted />
+        <Footer />
+    </main>)
 }
 export default App
---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar.jsx";
+import Hero from "./Hero.jsx";
+import Brands from "./Brands.jsx";
+import Service from "./Service.jsx";
+import Train from "./Train.jsx";
+import Reviews from "./Reviews.jsx";
+import Improvement from "./Improvement.jsx";
+import GetStarted from "./GetStarted.jsx";
+import Footer from "./Footer.jsx";
+
+export {
+    Navbar, Hero, Brands, Service, Train, Reviews, Improvement, GetStarted, Footer,
+}
